ea912bb7719d539ab6cd0cd6f430306cf725d6fc
Scott Tadman <tadman@postageapp.com>
2017-03-20 15:46:59 -0400

    Expanding tests for Skein::Client::Worker

--- test/unit/test_skein_worker.rb
+++ test/unit/test_skein_worker.rb
@@ -1,6 +1,14 @@
 require_relative '../helper'
 
-class TestSkeinWorker  < Test::Unit::TestCase
+class ExampleWorker < Skein::Client::Worker
+  attr_reader :initialized
+
+  def after_initialize
+    @initialized = true
+  end
+end
+
+class TestSkeinWorker < Test::Unit::TestCase
   def test_default
     client = Skein::Client.new
 
@@ -9,6 +17,20 @@ class TestSkeinWorker  < Test::Unit::TestCase
     assert worker
 
     assert_equal false, worker.async?
+
+  ensure
+    client and client.close
+  end
+
+  def test_subclass_initialize
+    client = Skein::Client.new
+
+    worker = ExampleWorker.new('test_queue', connection: client.connection)
+
+    assert worker
+
+    assert_equal false, worker.async?
+    assert_equal true, worker.initialized
 
   ensure
     client and client.close
